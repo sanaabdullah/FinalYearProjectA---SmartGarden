994f7de511c3cff14b31305f1c8a0348729ddc7f
Sana Faiz Abdullah <4323904@students.swinburne.edu.my>
2017-11-17 22:44:38 +0800

    ignore

--- SmartGarden/index1.php
+++ SmartGarden/index1.php
@@ -11,7 +11,7 @@
     <title>Welcome to Smart Garden</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="SmartGarden/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom fonts for this template -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
@@ -51,7 +51,7 @@
               <a class="nav-link text-uppercase text-expanded" href="login.php">Login/Register</a>
             </li>
             <li class="nav-item px-lg-4">
-              <a class="nav-link text-uppercase text-expanded" href="product.php">Shop Now</a>
+              <a class="nav-link text-uppercase text-expanded" href=" ">Shop Now</a>
             </li>
           </ul>
         </div>
@@ -124,8 +124,8 @@
     </footer>
 
     <!-- Bootstrap core JavaScript -->
-    <script src="SmartGarden/vendor/jquery/jquery.min.js"></script>
-    <script src="SmartGarden/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   </body>
 
